034f353fdb4f49e331c045070e99de51ee97d6be
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-06-20 19:43:52 +0000

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - lastdigit
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char alphabet;
+alphabet = 'a';
+while (alphabet <= 'z')
+{
+putchar (alphabet);
+alphabet++;
+}
+return (0);
+}
