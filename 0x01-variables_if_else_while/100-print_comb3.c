4a77f58580169201a4f1f55b9eaa2d89dbf5bfa4
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-06-21 03:34:57 +0000

    compilation

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,25 +1,25 @@
 #include <stdio.h>
 /**
- * main - combination
- * Return: 0
+ * main - compiled
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-int num1, num2, num3, num4;
-for (num1 = 0; num1 < 1000; num1++)
+int n, d;
+for (n = 48; n <= 57; n++)
 {
-num2 = num1 / 100;
-num3 = (num1 / 10) % 10; 
-num4 = num1 % 10; 
-if (num2 < num3 && num3 < num4)
+for (d = 48; d <= 57; d++)
 {
-putchar(num2 + '0');
-putchar(num3 + '0');
-putchar(num4 + '0');
-if (num1 < 700)
+if (n < d)
 {
-putchar(',');
-putchar(' ');
+putchar (n);
+putchar (d);
+if (n != 56 || d != 57)
+{
+putchar (44);
+putchar (' ');
+}
 }
 }
 }
