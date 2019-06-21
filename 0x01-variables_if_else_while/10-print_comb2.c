da542afd9ba5a19fefad35019a98439d1c76eb10
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-06-21 00:45:40 +0000

    number0-99

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,35 +1,21 @@
 #include <stdio.h>
 /**
- * main - print numbers from 00 to 99.
-(*
-* Return: 0 on success
-*/
+ * main - number0-99
+ * Return: 0
+ */
 int main(void)
 {
-int i = '0';
-int j = '0';
-while (i <= '9')
+int i = 0;
+while (i < 10)
 {
-while (j <= '9')
+putchar(i + '0');
+if (i < 9)
 {
-putchar(i);
-putchar(j);
-if (j == '9' && i == '9')
-{
-putchar('\n');
-}
-else
-{
-putchar(',');
-putchar(' ');
-}
-j++;
-}
-if (j >= '9')
-{
-j = '0';
+putchar(44);
+putchar(32);
 }
 i++;
 }
+putchar('\n');
 return (0);
 }
