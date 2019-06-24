9d6b444d3712085ae46d9301b874df15e5c63a8a
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-06-24 02:17:31 +0000

    combination3numbers

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 /**
- * main - compiled
+ * main - combination3numbers
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 int number1, number2, number3;
-
 for (number1 = 48; number1 <= 57; number1++)
 {
 for (number2 = 48; number2 <= 57; number2++)
@@ -19,7 +18,7 @@ if ((number1 < number2) && (number2 < number3) && (number1 < number3))
 putchar (number1);
 putchar (number2);
 putchar (number3);
- if ((number1 != 55) || (number2 != 56) || (number3 != 57))
+if ((number1 != 55) || (number2 != 56) || (number3 != 57))
 {
 putchar (44);
 putchar (' ');
