49c645f185b073544445270bc5311bcbdaa3b7a0
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-06-26 00:41:31 +0000

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,13 +2,15 @@
 /**
  * print_alphabet - alphabet list
  * @lt: variable
- * Return: chars
+ * Return 10
  */
-void print_alphabet(void lt)
+
+void print_alphabet(void)
 {
 	int lt;
 
 	for (lt = 97; lt <= 122; lt++)
-		_putchar (lt);
-	_putchar (10);
+		_putchar(lt);
+
+	_putchar(10);
 }
