4af9aa86bc0ffbb899a82c9ef986bced8c13e941
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-06-26 00:03:10 +0000

    Holberton

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,16 +6,15 @@
  */
 int main(void)
 {
-  _putchar(72);
-  _putchar(111);
-  _putchar(108);
-  _putchar(98);
-  _putchar(101);
-  _putchar(114);
-  _putchar(116);
-  _putchar(111);
-  _putchar(110);
-  _putchar(10);
-
-  return (0);
+_putchar(72);
+_putchar(111);
+_putchar(108);
+_putchar(98);
+_putchar(101);
+_putchar(114);
+_putchar(116);
+_putchar(111);
+_putchar(110);
+_putchar(10);
+return (0);
 }
