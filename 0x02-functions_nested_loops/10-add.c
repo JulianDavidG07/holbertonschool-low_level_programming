51d16d6fd48abcf5a48b50ed2669629bfb57e347
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-06-26 02:16:02 +0000

    sumintegers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+# include "holberton.h"
+
+/**
+ * add - last digit
+ *
+ * @nume1 @nume2: Variable
+ *
+ * Return: sum interger
+ */
+
+int add(int nume1, int nume2)
+{
+	return (nume1 + nume2);
+}
