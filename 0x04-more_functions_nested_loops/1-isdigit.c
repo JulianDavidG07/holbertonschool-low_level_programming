19c7ec7a82192af534f1030f2c047f83a4363fb6
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-06-27 17:26:35 +0000

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _isupper - function that checks for a digit (0 through 9).
+ * @c: variable
+ * Return: 1 if uppercase, else 0
+ */
+
+int _isdigit(int c)
+
+{
+	if (c >= 48 && c <= 57)
+
+		return (1);
+
+	return (0);
+}
