89b40872774a4e10e165a2497f7879b3b45a3520
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-07-08 22:19:20 +0000

    funtions

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,35 @@
+#include "holberton.h"
+
+/**
+ * _strpbrk - function locates the first occurrence in the string s
+ * @s: string
+ * @accept: char
+ * Return: 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, t, j;
+
+	i = 0;
+	while (s[i] != '\0')
+
+	{
+		t = 0;
+
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+				t = 1;
+		}
+
+		j = 0;
+
+		if (t == 1)
+
+			return (s + i);
+		i++;
+	}
+
+	return (0);
+}
