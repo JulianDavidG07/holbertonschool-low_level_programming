b236d7fa0a3df2a04d01368918a79ed3871448bf
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-07-09 02:42:11 +0000

    functions

--- 0x07-pointers_arrays_strings/9-set_string.c
+++ 0x07-pointers_arrays_strings/9-set_string.c
@@ -1,24 +1,14 @@
 #include "holberton.h"
-#include "stdio.h"
 
 /**
- * print_diagsums- sum diagonal values
- * @a: pointer to array of int type
- * @size: int type
- * Return: sum of two sides
+ * set_string - use a pointer to derefrence another string
+ * @s: pointer to pointer of string
+ * @to: pointer to string
+ * Return: always success
  *
  */
 
-void print_diagsums(int *a, int size)
+void set_string(char **s, char *to)
 {
-	int x, y, suma, sumb;
-
-	suma = 0;
-	sumb = 0;
-
-	for (x = 0;  x < size; x++)
-		suma += a[(size + 1) * x];
-	for (y =  0; y < size; y++)
-		sumb += a[(size - 1) * (y + 1)];
-	printf("%d, %d\n",  suma, sumb);
+	*s = to;
 }
