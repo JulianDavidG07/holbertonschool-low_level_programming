9a67f14d1c0297313c1d3e3b0bc5724b7cefd54a
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-07-24 22:09:45 +0000

    funtion_pointer

---
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -0,0 +1,21 @@
+#include "3-calc.h"
+#include <stdio.h>
+
+/**
+ * get_op_func - function to select operator
+ *
+ * @s: chat type
+ * Return: always successful
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	}
+}
