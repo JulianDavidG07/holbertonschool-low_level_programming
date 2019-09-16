98c9122931a4d3d0683b05c0ec1f2c7fa97b8766
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-09-16 00:11:39 -0500

    double list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,42 @@
+#include "lists.h"
+
+/**
+
+ * free_dlistint - Function that free a dlistint_t list.
+
+ * @head: head of the doubly linked list
+
+ * Return: void
+
+ */
+
+void free_dlistint(dlistint_t *head)
+
+{
+
+	dlistint_t *ptr;
+
+
+	if (head == NULL)
+
+		return;
+
+	ptr = head->next;
+
+	while (ptr != NULL)
+
+	{
+
+		free(head);
+
+		head = ptr;
+
+		ptr = ptr->next;
+
+	}
+
+	free(head);
+
+	free(ptr);
+
+}
