7e68c364ed83a789e0f4581e94493f97857cacc6
JulianDavidG07 <julygsaenz1995@hotmail.com>
2019-12-06 03:01:58 -0500

    delete

---
+++ 0x1D-binary_trees/3-binary_tree_delete.c
@@ -0,0 +1,12 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_delete - C function to delete a tree
+ * @tree: pointer to delete
+ * Return: return tree delete
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+if (tree == NULL)
+return (NULL);
+free(tree);
+}
